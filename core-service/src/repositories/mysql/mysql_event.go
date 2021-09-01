8cf9dc72958018e9b42f25192d3447c5d126348c
Novansyah <rachadian.novansyah15@gmail.com>
2021-09-01 14:18:51 +0700

    no longer use keywords on params

---
+++ core-service/src/repositories/mysql/mysql_event.go
@@ -0,0 +1,119 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/jabardigitalservice/portal-jabar-services/core-service/src/domain"
+	"github.com/sirupsen/logrus"
+)
+
+type mysqlEventRepository struct {
+	Conn *sql.DB
+}
+
+func NewMysqlEventRepository(Conn *sql.DB) domain.EventRepository {
+	return &mysqlEventRepository{Conn}
+}
+
+func (mr *mysqlEventRepository) fetchQuery(ctx context.Context, query string, args ...interface{}) (result []domain.Event, err error) {
+	rows, err := mr.Conn.QueryContext(ctx, query, args...)
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	defer func() {
+		errRow := rows.Close()
+		if errRow != nil {
+			logrus.Error(errRow)
+		}
+	}()
+
+	result = make([]domain.Event, 0)
+	for rows.Next() {
+		event := domain.Event{}
+		categoryID := int64(0)
+		err = rows.Scan(
+			&event.ID,
+			&categoryID,
+			&event.Title,
+			&event.Description,
+			&event.Date,
+			&event.Address,
+			&event.StartHour,
+			&event.EndHour,
+			&event.Image,
+			&event.PublishedBy,
+			&event.Province,
+			&event.City,
+			&event.District,
+			&event.Village,
+			&event.CreatedAt,
+			&event.UpdatedAt,
+		)
+
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+
+		event.Category = domain.Category{ID: categoryID}
+		result = append(result, event)
+	}
+
+	return result, nil
+}
+
+func (mr *mysqlEventRepository) count(ctx context.Context, query string) (total int64, err error) {
+
+	err = mr.Conn.QueryRow(query).Scan(&total)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	return total, nil
+}
+
+func (mr *mysqlEventRepository) Fetch(ctx context.Context, params *domain.Request) (res []domain.Event, total int64, err error) {
+	query := `SELECT id, category_id, title, description, date, address, start_hour, end_hour, image, published_by, province_code, city_code, district_code, village_code, created_at, updated_at FROM event`
+
+	if params.Keyword != "" {
+		query = query + ` WHERE title like '%` + params.Keyword + `%' `
+	}
+
+	if params.StartDate != "" && params.EndDate != "" {
+		query = query + ` WHERE (date BETWEEN ` + params.StartDate + ` AND ` + params.EndDate + `)`
+	}
+
+	query = query + ` ORDER BY created_at LIMIT ?,? `
+
+	res, err = mr.fetchQuery(ctx, query, params.Offset, params.PerPage)
+
+	if err != nil {
+		return nil, 0, err
+	}
+
+	total, _ = mr.count(ctx, "SELECT COUNT(1) FROM event")
+
+	return
+}
+
+func (mr *mysqlEventRepository) GetByID(ctx context.Context, id int64) (res domain.Event, err error) {
+	query := `SELECT id, category_id, title, description, date, address, start_hour, end_hour, image, published_by, province_code, city_code, district_code, village_code, created_at, updated_at FROM informations` + ` WHERE ID = ?`
+
+	list, err := mr.fetchQuery(ctx, query, id)
+	if err != nil {
+		return domain.Event{}, err
+	}
+
+	if len(list) > 0 {
+		res = list[0]
+	} else {
+		return res, domain.ErrNotFound
+	}
+
+	return
+}
