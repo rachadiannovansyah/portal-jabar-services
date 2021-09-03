8fbb5638b7408229453a3482344cacab5250c234
Khi Hady Sucahyo <khihady.ks@gmail.com>
2021-09-03 10:33:06 +0700

    update event handler

--- core-service/src/delivery/http/event_handler.go
+++ core-service/src/delivery/http/event_handler.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"math"
 	"net/http"
 	"strconv"
 
@@ -29,27 +28,7 @@ func (h *EventHandler) Fetch(c echo.Context) error {
 
 	ctx := c.Request().Context()
 
-	page, _ := strconv.ParseInt(c.QueryParam("page"), 10, 64)
-	perPage, _ := strconv.ParseInt(c.QueryParam("per_page"), 10, 64)
-
-	if page == 0 {
-		page = 1
-	}
-	if perPage == 0 {
-		perPage = 10
-	}
-
-	offset := (page - 1) * perPage
-
-	params := domain.Request{
-		Keyword:   c.QueryParam("q"),
-		PerPage:   perPage,
-		Offset:    offset,
-		OrderBy:   c.QueryParam("order_by"),
-		SortBy:    c.QueryParam("sort_by"),
-		StartDate: c.QueryParam("start_date"),
-		EndDate:   c.QueryParam("end_date"),
-	}
+	params := GetRequestParams(c)
 
 	listEvent, total, err := h.EventUcase.Fetch(ctx, &params)
 
@@ -57,15 +36,7 @@ func (h *EventHandler) Fetch(c echo.Context) error {
 		return c.JSON(getStatusCode(err), &ResponseError{Message: err.Error()})
 	}
 
-	res := &domain.ResultsData{
-		Data: listEvent,
-		Meta: &domain.MetaData{
-			TotalCount:  total,
-			TotalPage:   math.Ceil(float64(total) / float64(perPage)),
-			CurrentPage: page,
-			PerPage:     perPage,
-		},
-	}
+	res := Paginate(c, listEvent, total, params)
 
 	return c.JSON(http.StatusOK, res)
 }
