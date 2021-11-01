face68655eb3306bd7f7a0f5c0084d36f4c46667
Novansyah <rachadian.novansyah15@gmail.com>
2021-11-01 17:24:55 +0700

    considering to groupin in one file migration (on pre development)

--- core-service/src/database/migrations/1629281242_initialize_schema.up.sql
+++ core-service/src/database/migrations/1629281242_initialize_schema.up.sql
@@ -193,4 +193,23 @@ CREATE TABLE featured_programs (
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS tags;
+CREATE TABLE tags (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(20) NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE INDEX idx_tags_name ON tags (name);
+
+DROP TABLE IF EXISTS tags_data;
+CREATE TABLE tags_data (
+    id int(10) unsigned NOT NULL AUTO_INCREMENT,
+    data_id int(10) unsigned,
+    tags_id int(10) unsigned,
+    tags_name varchar(20),
+    type varchar(10),
+    PRIMARY KEY(id)
+);
+CREATE INDEX idx_tags_name ON tags_data (tags_name);
+
 COMMIT;
