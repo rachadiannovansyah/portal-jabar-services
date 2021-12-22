f51aa7e54ada69298cae6ab1b4ad6fbef7daf9b5
Khi Hady Sucahyo <khihady.ks@gmail.com>
2021-12-22 10:27:16 +0700

    remove min paswd rule on login request

--- core-service/src/domain/auth.go
+++ core-service/src/domain/auth.go
@@ -21,7 +21,7 @@ type JwtCustomRefreshClaims struct {
 
 type LoginRequest struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
+	Password string `json:"password" validate:"required"`
 }
 
 type RefreshRequest struct {
