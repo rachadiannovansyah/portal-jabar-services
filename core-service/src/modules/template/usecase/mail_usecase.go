5c8279f6b3a5799260dc412a497001a540d8f5c3
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-03-24 22:07:45 +0700

    adjust body with current user request

--- core-service/src/modules/template/usecase/mail_usecase.go
+++ core-service/src/modules/template/usecase/mail_usecase.go
@@ -2,24 +2,28 @@ package usecase
 
 import (
 	"context"
+	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/jabardigitalservice/portal-jabar-services/core-service/src/domain"
 )
 
 type mailTemplateUsecase struct {
 	mailRepo       domain.TemplateRepository
+	userRepo       domain.UserRepository
 	contextTimeout time.Duration
 }
 
-func NewMailTemplateUsecase(m domain.TemplateRepository, timeout time.Duration) domain.TemplateUsecase {
+func NewMailTemplateUsecase(m domain.TemplateRepository, u domain.UserRepository, timeout time.Duration) domain.TemplateUsecase {
 	return &mailTemplateUsecase{
 		mailRepo:       m,
+		userRepo:       u,
 		contextTimeout: timeout,
 	}
 }
 
-func (m *mailTemplateUsecase) GetByTemplate(ctx context.Context, key string) (res domain.Template, err error) {
+func (m *mailTemplateUsecase) GetByTemplate(ctx context.Context, id uuid.UUID, key string) (res domain.Template, err error) {
 	ctx, cancel := context.WithTimeout(ctx, m.contextTimeout)
 	defer cancel()
 
@@ -27,6 +31,18 @@ func (m *mailTemplateUsecase) GetByTemplate(ctx context.Context, key string) (re
 	if err != nil {
 		return
 	}
+
+	user, err := m.userRepo.GetByID(ctx, id)
+	if err != nil {
+		return
+	}
+
+	// append for response get account submission
+	bodyTmp := res.Body
+	bodyTmp = strings.ReplaceAll(bodyTmp, "{name}", user.Name)
+	bodyTmp = strings.ReplaceAll(bodyTmp, "{unitName}", user.UnitName)
+
+	res.Body = bodyTmp
 
 	return
 }
