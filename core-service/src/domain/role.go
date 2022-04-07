2ba0e475eeb316a4473c5464d254f4dd65941ba2
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-04-07 11:09:16 +0700

    const role moving to domain role.go

--- core-service/src/domain/role.go
+++ core-service/src/domain/role.go
@@ -5,6 +5,12 @@ import (
 	"time"
 )
 
+// Constanta Role
+const RoleSuperAdmin int8 = 1
+const RoleGroupAdmin int8 = 2
+const RoleAdministrator int8 = 3
+const RoleContributor int8 = 4
+
 // Role ...
 type Role struct {
 	ID          int8      `json:"id"`
