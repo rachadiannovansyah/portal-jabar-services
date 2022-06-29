630e1cc652f1b2e8ba5f818c0af398e8b7ea98b4
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-06-29 15:30:39 +0700

    fix issue start date when exceed the review time

--- core-service/src/helpers/setters.go
+++ core-service/src/helpers/setters.go
@@ -11,11 +11,16 @@ func SetPropLiveNews(news *domain.StoreNewsRequest) {
 
 	currentTime := time.Now()
 	startDate := ConvertStringToTime(news.StartDate)
+	endDate := ConvertStringToTime(news.EndDate)
 
-	// if startDate is less than equal to today, set live to true
-	if startDate.Unix() <= time.Now().Unix() {
+	// if endDate is exceed current time, set it to published at + duration
+	if time.Now().Unix() > endDate.Unix() {
+		news.StartDate = ConvertTimeToString(currentTime)
+		news.EndDate = ConvertTimeToString(currentTime.AddDate(0, 0, int(news.Duration)))
+		news.IsLive = 1
+		news.PublishedAt = &currentTime
+	} else if startDate.Unix() <= time.Now().Unix() {
 		news.IsLive = 1
 		news.PublishedAt = &currentTime
 	}
-
 }
