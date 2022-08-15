140950b3e25fab3622d14485074dac9aee29d8f1
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-08-15 15:53:00 +0700

    remove emoji from the request

--- core-service/src/modules/feedback/usecase/feedback_ucase.go
+++ core-service/src/modules/feedback/usecase/feedback_ucase.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/forPelevin/gomoji"
 	"github.com/jabardigitalservice/portal-jabar-services/core-service/src/domain"
 	"github.com/mrz1836/go-sanitize"
 )
@@ -24,6 +25,12 @@ func NewFeedbackUsecase(f domain.FeedbackRepository, timeout time.Duration) doma
 func (u *feedbackUsecase) Store(c context.Context, f *domain.Feedback) (err error) {
 	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
 	defer cancel()
+
+	// detects and remove if emojis contained on the request
+	f.Compliments = gomoji.RemoveEmojis(f.Compliments)
+	f.Criticism = gomoji.RemoveEmojis(f.Criticism)
+	f.Suggestions = gomoji.RemoveEmojis(f.Suggestions)
+	f.Sector = gomoji.RemoveEmojis(f.Sector)
 
 	// sanitizing the request from script attack
 	f.Compliments = sanitize.Scripts(f.Compliments)
