82fd5404240a91dbab689f9e6ceda3cd94decf5a
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-08-25 14:43:22 +0700

    add new migration column document archive

---
+++ core-service/src/database/migrations/20220825022241_create_document_archives_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE document_archives (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(150) NOT NULL,
+    excerpt TEXT NOT NULL,
+    description TEXT NOT NULL,
+    source VARCHAR(80) DEFAULT NULL,
+    mimetype VARCHAR(30) DEFAULT NULL,
+    category VARCHAR(50) NOT NULL,
+    created_by VARCHAR(36) DEFAULT NULL,
+    updated_by VARCHAR(36) DEFAULT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+CREATE INDEX idx_title_documents ON document_archives (title);
+CREATE INDEX idx_category_documents ON document_archives (category);
