b313b2c90d988a058e83960c70f091df7e882ed7
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-09-12 18:16:44 +0700

    update logic set prop live news

--- core-service/src/helpers/setters.go
+++ core-service/src/helpers/setters.go
@@ -7,15 +7,24 @@ import (
 )
 
 // SetPropLiveNews ...
-func SetPropLiveNews(news *domain.StoreNewsRequest) {
+func SetPropLiveNews(newsRequest *domain.StoreNewsRequest) {
 
 	currentTime := time.Now()
-	endDate := ConvertStringToTime(news.EndDate)
-	news.IsLive = 1
-	news.PublishedAt = &currentTime
+	endDate := ConvertStringToTime(newsRequest.EndDate)
+	startDate := ConvertStringToTime(newsRequest.StartDate)
+	newsRequest.IsLive = 0
+
 	// if endDate is exceed current time, set it to published at + duration
 	if time.Now().Unix() > endDate.Unix() {
-		news.StartDate = ConvertTimeToString(currentTime)
-		news.EndDate = ConvertTimeToString(currentTime.AddDate(0, 0, int(news.Duration)))
+		newsRequest.StartDate = ConvertTimeToString(currentTime)
+		newsRequest.EndDate = ConvertTimeToString(currentTime.AddDate(0, 0, int(newsRequest.Duration)))
+		newsRequest.IsLive = 1
+		newsRequest.PublishedAt = &currentTime
+	}
+
+	// if startDate is less than equal to today, set live to true
+	if startDate.Unix() <= time.Now().Unix() {
+		newsRequest.IsLive = 1
+		newsRequest.PublishedAt = &currentTime
 	}
 }
