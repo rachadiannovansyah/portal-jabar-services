dde0a4cf500d7487da986f2b3de65fdc9ab0e99e
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-09-13 10:40:12 +0700

    set live and publish func

--- core-service/src/helpers/setters.go
+++ core-service/src/helpers/setters.go
@@ -10,6 +10,7 @@ import (
 func SetPropLiveNews(newsRequest *domain.StoreNewsRequest) {
 
 	currentTime := time.Now()
+	isLive := int8(1)
 	endDate := ConvertStringToTime(newsRequest.EndDate)
 	startDate := ConvertStringToTime(newsRequest.StartDate)
 	newsRequest.IsLive = 0
@@ -18,13 +19,23 @@ func SetPropLiveNews(newsRequest *domain.StoreNewsRequest) {
 	if time.Now().Unix() > endDate.Unix() {
 		newsRequest.StartDate = ConvertTimeToString(currentTime)
 		newsRequest.EndDate = ConvertTimeToString(currentTime.AddDate(0, 0, int(newsRequest.Duration)))
-		newsRequest.IsLive = 1
-		newsRequest.PublishedAt = &currentTime
+		setLiveAndPublish(newsRequest, isLive, currentTime)
 	}
 
 	// if startDate is less than equal to today, set live to true
 	if startDate.Unix() <= time.Now().Unix() {
-		newsRequest.IsLive = 1
-		newsRequest.PublishedAt = &currentTime
+		setLiveAndPublish(newsRequest, isLive, currentTime)
 	}
+}
+
+func setLiveAndPublish(newsRequest *domain.StoreNewsRequest, args ...interface{}) *domain.StoreNewsRequest {
+	// type assertion params
+	isLive := args[0].(int8)
+	publishedAt := args[1].(time.Time)
+
+	// set it to struct store request
+	newsRequest.IsLive = isLive
+	newsRequest.PublishedAt = &publishedAt
+
+	return newsRequest
 }
