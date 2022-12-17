09d55aa7796b1868a5ab2072dea7859a9a3c8dd5
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-12-17 23:17:42 +0700

    fix error build docker on db pool time connection

--- core-service/src/utils/connection.go
+++ core-service/src/utils/connection.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -39,6 +40,10 @@ func initMysql(cfg *config.Config) *sql.DB {
 	db.SetMaxIdleConns(cfg.DB.MaxIdleConns)
 	// Maximum Open Connections
 	db.SetMaxOpenConns(cfg.DB.MaxOpenConns)
+	// Idle Connection Timeout
+	db.SetConnMaxIdleTime(1 * time.Second)
+	// Connection Lifetime
+	db.SetConnMaxLifetime(30 * time.Second)
 
 	if err != nil {
 		log.Fatal(err)
