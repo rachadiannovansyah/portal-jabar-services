07a3e4d18750035a6c50fe466dcb8edd9e68958c
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2022-12-22 16:30:01 +0700

    init custom logger and middleware

---
+++ core-service/src/middleware/logger.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (m *GoMiddleware) Logging(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		m.Logger.Info(logrus.Fields(logrus.Fields{
+			"at":     time.Now().Format("2006-01-02 15:04:05"),
+			"method": c.Request().Method,
+			"uri":    c.Request().URL.String(),
+			"ip":     c.Request().RemoteAddr,
+		}), "incoming an request")
+		return next(c)
+	}
+}
