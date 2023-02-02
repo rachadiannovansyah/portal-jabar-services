2ea2305951cda7b21792195442541dffb27a5207
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-02-02 11:52:43 +0700

    update repo mock

--- core-service/src/domain/mocks/repositories/PopUpBannerRepositoryMocks.go
+++ core-service/src/domain/mocks/repositories/PopUpBannerRepositoryMocks.go
@@ -14,6 +14,27 @@ type PopUpBannerRepository struct {
 	mock.Mock
 }
 
+// CheckStatus provides a mock function with given fields: ctx, status
+func (_m *PopUpBannerRepository) CheckStatus(ctx context.Context, status string) (int64, error) {
+	ret := _m.Called(ctx, status)
+
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(context.Context, string) int64); ok {
+		r0 = rf(ctx, status)
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string) error); ok {
+		r1 = rf(ctx, status)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Delete provides a mock function with given fields: ctx, id
 func (_m *PopUpBannerRepository) Delete(ctx context.Context, id int64) error {
 	ret := _m.Called(ctx, id)
