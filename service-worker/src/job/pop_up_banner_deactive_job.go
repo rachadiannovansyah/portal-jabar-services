f5568da260e14fbf08e37082f4b8575d7be86f05
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-02-06 14:39:46 +0700

    optimize: query deactivate job

--- service-worker/src/job/pop_up_banner_deactive_job.go
+++ service-worker/src/job/pop_up_banner_deactive_job.go
@@ -11,47 +11,27 @@ import (
 func PopUpBannerDeactivateJob(conn *utils.Conn, cfg *config.Config) {
 	logrus.Println("PopUpBannerDeactivateJob is running...")
 
-	// get banners id will be deactivated
-	var IDs string
-
-	query := `SELECT id FROM pop_up_banners
-		WHERE status = 'ACTIVE'
-		AND end_date < NOW()`
-
-	rows, err := conn.Mysql.Query(query)
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-
-	for rows.Next() {
-		var id string
-		err = rows.Scan(&id)
-		if err != nil {
-			logrus.Error(err)
-			return
-		}
-		IDs = id
-	}
-
 	// deactivate banner
-	deactivateQuery := `UPDATE pop_up_banners SET status = ? WHERE id = ?`
+	deactivateQuery := `UPDATE pop_up_banners 
+	SET status = 'NON-ACTIVE' 
+	WHERE status = 'ACTIVE'
+	AND end_date < NOW()`
+
 	stmt, err := conn.Mysql.PrepareContext(context.TODO(), deactivateQuery)
 	if err != nil {
 		logrus.Error(err)
 	}
 
-	_, err = stmt.ExecContext(context.TODO(),
-		"NON-ACTIVE",
-		IDs,
-	)
+	res, err := stmt.ExecContext(context.TODO())
 	if err != nil {
 		logrus.Error(err)
 	}
 
+	rowAffected, _ := res.RowsAffected()
+
 	// just printed out job activities
-	if IDs != "" {
-		logrus.Println("PopUpBannerDeactivateJob: Deactivate banner with id: ", IDs)
+	if rowAffected != 0 {
+		logrus.Println("PopUpBannerDeactivateJob: Deactivate banner row affected: ", rowAffected)
 	} else {
 		logrus.Println("PopUpBannerDeactivateJob: No-one active pop up banner")
 	}
