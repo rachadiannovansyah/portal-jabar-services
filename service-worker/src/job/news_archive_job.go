7d931fc5c109ff0165a320bbd3c2a335793019d4
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-02-08 11:13:23 +0700

    feat(service-worker): fix panic instance service worker

--- service-worker/src/job/news_archive_job.go
+++ service-worker/src/job/news_archive_job.go
@@ -39,64 +39,66 @@ func NewsArchiveJob(conn *utils.Conn, cfg *config.Config) {
 		newsIDs = append(newsIDs, id)
 	}
 
-	// Archive news
-	updateQuery := fmt.Sprintf(`UPDATE news SET is_live=0, status='ARCHIVED' WHERE id IN (%s)`, strings.Join(newsIDs, ","))
-	res, err := conn.Mysql.Exec(updateQuery)
-	if err != nil {
-		logrus.Error(err)
-	}
+	if len(newsIDs) > 0 {
+		// Archive news
+		updateQuery := fmt.Sprintf(`UPDATE news SET is_live=0, status='ARCHIVED' WHERE id IN (%s)`, strings.Join(newsIDs, ","))
+		res, err := conn.Mysql.Exec(updateQuery)
+		if err != nil {
+			logrus.Error(err)
+		}
 
-	// // rows affected
-	if ra, err := res.RowsAffected(); err != nil {
-		logrus.Error("ErrNewsArchiveJob: ", err)
-	} else {
-		logrus.Println("NewsArchiveJob: Rows affected: ", ra)
-	}
+		// rows affected
+		if ra, err := res.RowsAffected(); err != nil {
+			logrus.Error("ErrNewsArchiveJob: ", err)
+		} else {
+			logrus.Println("NewsArchiveJob: Rows affected: ", ra)
+		}
 
-	// elasticapi update body set is_live=0 where body id in (news ids) and domain=news
-	doc := map[string]interface{}{
-		"query": map[string]interface{}{
-			"bool": map[string]interface{}{
-				"must": []map[string]interface{}{
-					{
-						"terms": map[string]interface{}{
-							"id": newsIDs,
+		// elasticapi update body set is_live=0 where body id in (news ids) and domain=news
+		doc := map[string]interface{}{
+			"query": map[string]interface{}{
+				"bool": map[string]interface{}{
+					"must": []map[string]interface{}{
+						{
+							"terms": map[string]interface{}{
+								"id": newsIDs,
+							},
 						},
-					},
-					{
-						"match": map[string]interface{}{
-							"domain": "news",
+						{
+							"match": map[string]interface{}{
+								"domain": "news",
+							},
 						},
 					},
 				},
 			},
-		},
-		"script": map[string]interface{}{
-			"source": "ctx._source.is_active=params.is_active;",
-			"lang":   "painless",
-			"params": map[string]interface{}{
-				"is_active": false,
+			"script": map[string]interface{}{
+				"source": "ctx._source.is_active=params.is_active;",
+				"lang":   "painless",
+				"params": map[string]interface{}{
+					"is_active": false,
+				},
 			},
-		},
+		}
+
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(doc); err != nil {
+			logrus.Error("Error encoding doc", err)
+		}
+
+		// update elasticsearch
+		esRes, esErr := conn.Elastic.UpdateByQuery(
+			[]string{cfg.ELastic.IndexContent},
+			conn.Elastic.UpdateByQuery.WithBody(&buf),
+			conn.Elastic.UpdateByQuery.WithContext(context.Background()),
+		)
+
+		if esErr != nil {
+			logrus.Error("Error Update response", err)
+		}
+		defer esRes.Body.Close()
+		fmt.Println(esRes.String())
+
+		return
 	}
-
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(doc); err != nil {
-		logrus.Error("Error encoding doc", err)
-	}
-
-	// update elasticsearch
-	esRes, esErr := conn.Elastic.UpdateByQuery(
-		[]string{cfg.ELastic.IndexContent},
-		conn.Elastic.UpdateByQuery.WithBody(&buf),
-		conn.Elastic.UpdateByQuery.WithContext(context.Background()),
-	)
-
-	if esErr != nil {
-		logrus.Error("Error Update response", err)
-	}
-	defer esRes.Body.Close()
-	fmt.Println(esRes.String())
-
-	return
 }
--- service-worker/src/job/news_publishing_job.go
+++ service-worker/src/job/news_publishing_job.go
@@ -37,64 +37,66 @@ func NewsPublishingJob(conn *utils.Conn, cfg *config.Config) {
 		newsIDs = append(newsIDs, id)
 	}
 
-	// Publishing news
-	updateQuery := fmt.Sprintf(`UPDATE news SET is_live=1, published_at = now() WHERE id IN (%s)`, strings.Join(newsIDs, ","))
-	res, err := conn.Mysql.Exec(updateQuery)
-	if err != nil {
-		logrus.Error(err)
-	}
+	if len(newsIDs) > 0 {
+		// Publishing news
+		updateQuery := fmt.Sprintf(`UPDATE news SET is_live=1, published_at = now() WHERE id IN (%s)`, strings.Join(newsIDs, ","))
+		res, err := conn.Mysql.Exec(updateQuery)
+		if err != nil {
+			logrus.Error(err)
+		}
 
-	// // rows affected
-	if ra, err := res.RowsAffected(); err != nil {
-		logrus.Error("ErrNewsPublishingJob: ", err)
-	} else {
-		logrus.Println("NewsPublishingJob: Rows affected: ", ra)
-	}
+		// // rows affected
+		if ra, err := res.RowsAffected(); err != nil {
+			logrus.Error("ErrNewsPublishingJob: ", err)
+		} else {
+			logrus.Println("NewsPublishingJob: Rows affected: ", ra)
+		}
 
-	// elasticapi update body set is_live=0 where body id in (news ids) and domain=news
-	doc := map[string]interface{}{
-		"query": map[string]interface{}{
-			"bool": map[string]interface{}{
-				"must": []map[string]interface{}{
-					{
-						"terms": map[string]interface{}{
-							"id": newsIDs,
+		// elasticapi update body set is_live=0 where body id in (news ids) and domain=news
+		doc := map[string]interface{}{
+			"query": map[string]interface{}{
+				"bool": map[string]interface{}{
+					"must": []map[string]interface{}{
+						{
+							"terms": map[string]interface{}{
+								"id": newsIDs,
+							},
 						},
-					},
-					{
-						"match": map[string]interface{}{
-							"domain": "news",
+						{
+							"match": map[string]interface{}{
+								"domain": "news",
+							},
 						},
 					},
 				},
 			},
-		},
-		"script": map[string]interface{}{
-			"source": "ctx._source.is_active=params.is_active;",
-			"lang":   "painless",
-			"params": map[string]interface{}{
-				"is_active": true,
+			"script": map[string]interface{}{
+				"source": "ctx._source.is_active=params.is_active;",
+				"lang":   "painless",
+				"params": map[string]interface{}{
+					"is_active": true,
+				},
 			},
-		},
+		}
+
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(doc); err != nil {
+			logrus.Error("Error encoding doc", err)
+		}
+
+		// update elasticsearch
+		esRes, esErr := conn.Elastic.UpdateByQuery(
+			[]string{cfg.ELastic.IndexContent},
+			conn.Elastic.UpdateByQuery.WithBody(&buf),
+			conn.Elastic.UpdateByQuery.WithContext(context.Background()),
+		)
+
+		if esErr != nil {
+			logrus.Error("Error Update response", err)
+		}
+		defer esRes.Body.Close()
+		fmt.Println(esRes.String())
+
+		return
 	}
-
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(doc); err != nil {
-		logrus.Error("Error encoding doc", err)
-	}
-
-	// update elasticsearch
-	esRes, esErr := conn.Elastic.UpdateByQuery(
-		[]string{cfg.ELastic.IndexContent},
-		conn.Elastic.UpdateByQuery.WithBody(&buf),
-		conn.Elastic.UpdateByQuery.WithContext(context.Background()),
-	)
-
-	if esErr != nil {
-		logrus.Error("Error Update response", err)
-	}
-	defer esRes.Body.Close()
-	fmt.Println(esRes.String())
-
-	return
 }
