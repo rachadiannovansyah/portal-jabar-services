2a8871ded6ea9efb1e3a6161a655a5e9c01fb7c5
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-03-02 14:22:02 +0700

    core-svc-feat(mds): add main_service repo

---
+++ core-service/src/domain/main_service.go
@@ -0,0 +1,35 @@
+package domain
+
+import "context"
+
+type MainService struct {
+	ID                  int64      `json:"id"`
+	OpdName             int64      `json:"opd_name"`
+	GovernmentAffair    string     `json:"government_affair"`
+	SubGovernmentAffair string     `json:"sub_government_affair"`
+	ServiceForm         string     `json:"form"`
+	ServiceType         string     `json:"type"`
+	SubServiceType      string     `json:"sub_service_type"`
+	ServiceName         string     `json:"name"`
+	ProgramName         string     `json:"program_name"`
+	Description         string     `json:"description"`
+	ServiceUser         string     `json:"user"`
+	SubServiceSpbe      string     `json:"sub_service_spbe"`
+	OperationalStatus   string     `json:"operational_status"`
+	Technical           string     `json:"technical"`
+	Benefits            string     `json:"benefits"`
+	Facilities          string     `json:"facilities"`
+	Website             string     `json:"website"`
+	Links               NullString `json:"links"`
+	TermsAndConditions  string     `json:"terms_and_conditions"`
+	ServiceProcedures   string     `json:"service_procedures"`
+	ServiceFee          string     `json:"service_fee"`
+	OperationalTimes    NullString `json:"operational_times"`
+	HotlineNumber       string     `json:"hotline_number"`
+	HotlineMail         string     `json:"hotline_mail"`
+	Locations           NullString `json:"locations"`
+}
+
+type MainServiceRepository interface {
+	Store(ctx context.Context, body *StoreMasterDataService) (ID int64, err error)
+}
