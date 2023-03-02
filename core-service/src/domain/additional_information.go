e24960d58bb8f40251e965da2b5baa8556a39cca
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-03-02 14:22:34 +0700

    core-svc-feat(mds): add mds additional information repo

---
+++ core-service/src/domain/additional_information.go
@@ -0,0 +1,15 @@
+package domain
+
+import "context"
+
+type AdditionalInformation struct {
+	ID              int64       `json:"id"`
+	ResponsibleName string      `json:"responsible_name"`
+	PhoneNumber     string      `json:"phone_number"`
+	Email           string      `json:"email"`
+	SocialMedia     SocialMedia `json:"social_media"`
+}
+
+type AdditionalInformationRepository interface {
+	Store(ctx context.Context, body *StoreMasterDataService) (ID int64, err error)
+}
