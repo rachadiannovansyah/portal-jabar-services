9e61da03dddf2d4d36abfcede175c99179d9ac6f
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-03-29 11:54:38 +0700

    add migration alter column varchar instead of json

---
+++ core-service/src/database/migrations/20230329044905_alter_main_services_table.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE main_services
+MODIFY COLUMN facilities varchar(255),
+MODIFY COLUMN benefits varchar(255),
+MODIFY COLUMN terms_and_condition varchar(255),
+MODIFY COLUMN service_procedures varchar(255);
---
+++ core-service/src/database/migrations/20230329044905_alter_main_services_table.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE main_services
+MODIFY COLUMN facilities json,
+MODIFY COLUMN benefits json,
+MODIFY COLUMN terms_and_condition json,
+MODIFY COLUMN service_procedures json;
