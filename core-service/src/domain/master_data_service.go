4bc8e4282fdea7daaebd7f438bf0ab1aa8cf5dc0
Rachadian Novansyah <rachadian.novansyah15@gmail.com>
2023-04-06 13:34:10 +0700

    add list archive mds without paginate

--- core-service/src/domain/master_data_service.go
+++ core-service/src/domain/master_data_service.go
@@ -231,6 +231,7 @@ type MasterDataServiceUsecase interface {
 	GetByID(ctx context.Context, ID int64) (res MasterDataService, err error)
 	Update(context.Context, *StoreMasterDataService, int64) (err error)
 	TabStatus(context.Context) ([]TabStatusResponse, error)
+	Archive(ctx context.Context, params *Request) (res []MasterDataService, err error)
 }
 
 type MasterDataServiceRepository interface {
